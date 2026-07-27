c8044e86e8a2ba6d114a82c722f7cac0aefa12c0
daviw185-svg <daviw185@gmail.com>
2026-07-27 18:02:51 -0300

    Atividade 2

---
+++ Atividades/atividade2.py
@@ -0,0 +1,8 @@
+dados = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+print(f'a) Primeiros 3: {dados[:3]}')
+print(f'b) Últimos 3: {dados[-3:]}')
+print(f'c) Índices pares: {dados[1::2]}')
+print(f'd) Índices ímpares: {dados[0::2]}')
+print(f'e) Invertida: {dados[::-1]}')
+print(f'f) Elementos do meio: {dados[3:7]} ')    
