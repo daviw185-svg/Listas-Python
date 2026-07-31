3146526f32cc9942047e0c836e99efee69e0240c
daviw185-svg <daviw185@gmail.com>
2026-07-31 17:42:01 -0300

    Alteração de pastas

--- Exemplo_lista1/main.py
+++
@@ -1,9 +0,0 @@
-notas = [8.5, 7.0, 9.2, 6.8, 10.0]
-
-print(notas)
-print(notas[0]) # Primeiro
-print(notas[-1]) # Último
-print(notas[-2]) # Penúltimo
-
-notas[1] = 7.5 #Modificando item
-print(notas[1])
