3384535856375056624b380b6ca0c8011b6793ec
daviw185-svg <daviw185@gmail.com>
2026-08-07 17:29:02 -0300

    Alteração no print

--- Dicionarios-Python/Atividades/atv_all.py
+++ Dicionarios-Python/Atividades/atv_all.py
@@ -2,7 +2,7 @@ nome = input('Nome do Aluno: ')
 media = int(input('Digite a média da nota do aluno: '))
 frequencia = float(input('Qual a frequência do aluno(de 0 a 1, exemplo: 0.85): '))
 
-print(f'{nome} | {media} | {frequencia}')
+print(f'Elemento: {nome} | {media} | {frequencia}')
 
 if media >= 7 and frequencia >= 0.75:
     finalBom = print('Aprovado')
