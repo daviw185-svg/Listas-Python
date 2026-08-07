a1364630a17561d95c515609855edbe97c22f808
daviw185-svg <daviw185@gmail.com>
2026-08-07 17:27:15 -0300

    Atividade Geral

--- Dicionarios-Python/Atividades/atv_all.py
+++ Dicionarios-Python/Atividades/atv_all.py
@@ -1,9 +1,10 @@
+nome = input('Nome do Aluno: ')
 media = int(input('Digite a média da nota do aluno: '))
 frequencia = float(input('Qual a frequência do aluno(de 0 a 1, exemplo: 0.85): '))
 
-def aprovation():
-    if media >= 7, frequencia >= 0.75:
-        return 'Aprovado'
-    elif media < 7: 
-        return
-        
+print(f'{nome} | {media} | {frequencia}')
+
+if media >= 7 and frequencia >= 0.75:
+    finalBom = print('Aprovado')
+else : 
+    finalRuim = print('Reprovado')
